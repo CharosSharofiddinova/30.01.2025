48553d70e3b25b073e12700872cedf1e4a7d0444
Charos Sharofiddinova <c.sharofiddinova@172-10-61-91.lightspeed.moblal.sbcglobal.net>
2025-01-30 12:36:23 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,138 @@
+//problem1
+// #include <iostream>
+// using namespace std;
+// int main() {
+//    int sum=0,num;
+//     cout<<"Enter 5 numbers:";
+//     for (int i=0;i<5;i++) {
+//         cin>>num;
+//         sum+=num;
+//     }
+//     cout<<"Sum of the 5 numbers is:"<<sum<<endl;
+//     return 0;
+// }
+
+
+//problem2
+// #include<iostream>;
+// using namespace std;
+// int main () {
+//     int student_id, study_year, student_fee;
+//     double student_gpa;
+//     cout <<"What is your Student ID?";
+//     cin>>student_id;
+//     cout<<"what is your Year of Study?";
+//     cin>>study_year;
+//     cout<<"What is your fee?";
+//     cin>>student_fee;
+//     cout<<"What is your GPA?";
+//     cin>>student_gpa;
+//     cout<<"Student ID: "<<student_id<<"\nStudent Year of Study: "<<study_year
+//     << "\nStudent Fee of Study: "<<student_fee << "\nStudent GPA: "<<student_gpa;
+//     return 0;
+// }
+
+
+//problem3
+// #include<iostream>;
+// using namespace std;
+// int main () {
+//     int fahrenheit;
+//     cout<<"Farhenheit: ";
+//     cin>>fahrenheit;
+//     double celsius=(fahrenheit-32)/1.8;
+//     cout<<celsius<<endl;
+//     return 0;
+// }
+
+
+
+
+//problem4
+// #include<iostream>;
+// using namespace std;
+// int main () {
+//     int quantity;
+//     double price, total_price;
+//     cout<<"What is your Quantity?";
+//     cin>>quantity;
+//     cout<<"What is your Price?";
+//     cin>>price;
+//     total_price=quantity*price;
+//     cout<<"Your Total Price is: "<<total_price<<endl;
+//     return 0;
+// }
+
+
+
+
+//problem5
+// #include<iostream>;
+// using namespace std;
+// int main () {
+//     int total_age=0,age;
+//     cout<<"Enter ages of your 4 family members:";
+//     for (int i=0;i<4;i++) {
+//     cin>>age;
+//     total_age+=age;
+//     }
+//     double average_age=total_age/4;
+//     cout<<"Average age of your family is: "<<average_age<<endl;
+//     return 0;
+// }
+
+
+//problem6
+// #include<iostream>;
+// using namespace std;
+// int main () {
+//     int length, width;
+//     cout<<"enter the length: ";
+//     cin>>length;
+//     cout<<"Enter the width: ";
+//     cin>>width;
+//     double area=(length*width);
+//     double perimeter=2*(length+width);
+//     cout<<"Area is: "<<area<<endl;
+//     cout<<"Perimeter is: "<<perimeter<<endl;
+//     return 0;
+// }
+
+//problem7
+// #include<iostream>;
+// using namespace std;
+// int main () {
+//     int dividend, divisor,quotient, remainder;
+//     cout<<"Enter your dividend: ";
+//     cin>>dividend;
+//     cout<<"Enter your divisor: ";
+//     cin>>divisor;
+//     quotient=dividend/divisor;
+//     remainder=dividend%divisor;
+//     cout<<"The quotient of division is: "<<quotient<<"\nThe remainder of the division is: "<<remainder<<endl;
+//     return 0;
+// }
+
+
+//problem8
+// #include<iostream>;
+// using namespace std;
+// int main() {
+//     int us_dollar,exchange_rate,uz_som;
+//     cout<<"Enter amount in dollars: ";
+//     cin>>us_dollar;
+//     cout<<"enter exchange rate: ";
+//     cin>>exchange_rate;
+//     uz_som=us_dollar*exchange_rate;
+//     cout<<"The equivalent amount in UZS is: "<<uz_som<<endl;
+//     return 0;
+// }
+
+
+//problem9
+#include <iostream>;
+using namespace std;
+int main() {
+    int radius;
+    return 0;
+}
